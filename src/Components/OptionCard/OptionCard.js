2593f84b2ef685b9478c00735638c0c365bbae37
nayem9b <razwanniam1@gmail.com>
2022-10-11 05:13:21 +0600

    eye added

--- src/Components/OptionCard/OptionCard.js
+++ src/Components/OptionCard/OptionCard.js
@@ -1,10 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./OptionCard.css";
 
 const OptionCard = ({ optiondata }) => {
-  const { first, second, third, fourth } = optiondata;
   return (
     <div>
+      {/* <FontAwesomeIcon icon='fa-solid fa-eye' /> */}
+
       <div className='position form-control flex'>
         {optiondata}
         <label className='label cursor-pointer flex'>
@@ -14,6 +16,7 @@ const OptionCard = ({ optiondata }) => {
             name='radio-6'
             className='radio checked:bg-blue-500'
             checked
+            onClick={() => console.log("clicked")}
           />
         </label>
       </div>
--- src/Components/QuizCard/QuizCard.js
+++ src/Components/QuizCard/QuizCard.js
@@ -1,13 +1,18 @@
 import React from "react";
 import OptionCard from "../OptionCard/OptionCard";
-
+import { faEye } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const QuizCard = ({ reletedqdata }) => {
-  console.log(reletedqdata);
-  const { question, options } = reletedqdata;
+  const { question, options, correctAnswer } = reletedqdata;
+ const 
   return (
     <div>
       <div className=' grid my-5 card w-[900px] bg-base-100 shadow-xl'>
         <div className='card-body'>
+          <button onClick={() => console.log("eye clicked")}>
+            <FontAwesomeIcon className='mt-2' icon={faEye} />
+          </button>
+
           <h2 className='card-title text-purple-500'>{question}</h2>
 
           {options.map((optiondata) => (
