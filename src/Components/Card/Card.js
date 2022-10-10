6a7a6013c3b349d3dcdbd9d0825b0a13fa21ed0b
nayem9b <razwanniam1@gmail.com>
2022-10-10 23:57:09 +0600

    header added and card fixed

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,23 +1,19 @@
 import React from "react";
 
-const Card = ({ logo, name, total }) => {
+const Card = ({ quizdt }) => {
+  const { logo, name, total } = quizdt;
+  console.log(quizdt);
+
   return (
-    <div>
-      <div className='card w-96 bg-base-100 shadow-xl'>
-        <figure className='px-10 pt-10'>
-          <img
-            src='https://placeimg.com/400/225/arch'
-            alt='Shoes'
-            className='rounded-xl'
-          />
-        </figure>
-        <div className='card-body items-center text-center'>
-          <h2 className='card-title'>Shoes! {name}</h2>
-          <h2> bello {total}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className='card-actions'>
-            <button className='btn btn-primary'>Buy Now</button>
-          </div>
+    <div className='card w-96 bg-base-100 shadow-xl'>
+      <figure className='px-10 pt-10'>
+        <img src={logo} alt='Shoes' className='rounded-xl' />
+      </figure>
+      <div className='card-body items-center text-center'>
+        <h2 className='card-title'>{name}</h2>
+        <p>Total Quiz: {total}</p>
+        <div className='card-actions'>
+          <button className='btn btn-primary'>Start Quiz</button>
         </div>
       </div>
     </div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Card from "../Card/Card";
+import Header from "../Header/Header";
 
 const Home = () => {
   const loaderData = useLoaderData();
   const QuizData = loaderData.data;
   return (
     <div>
-      <h1> hello im from home</h1>
+      <Header></Header>
       {QuizData.map((quizdt) => (
-        <Card key={quizdt.id} quizdt={quizdt}></Card>
+        <Card key={quizdt.total} quizdt={quizdt}></Card>
       ))}
-      <Card></Card>
     </div>
   );
 };
