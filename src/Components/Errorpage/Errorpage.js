2a64a8be344c2720538c00b25edbd735267e11b7
nayem9b <razwanniam1@gmail.com>
2022-10-12 01:14:59 +0600

    error page added

--- src/Components/Errorpage/Errorpage.js
+++ src/Components/Errorpage/Errorpage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import error from "../../images/97041-error-page.json";
+import Lottie from "lottie-react";
 
 const Errorpage = () => {
   return (
     <div>
-      <h1>Your got an errro</h1>
+      <div className=' flex justify-center lg:ml-[400px] lg:mt-[-66px] lg:h-[800px] sm:h-[100px] sm:w-[300px] lg:w-[800px]'>
+        <Lottie animationData={error} loop={true} />
+      </div>
     </div>
   );
 };
